b7bac1478c5c6e001e33267047f1f2cbf5bdb07e
Bento Netto <bentoeemanuele@gmail.com>
2024-06-24 15:35:09 -0300

    Configurei o Nodemon com o TypeScript

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express from 'express'
 const servidor = express()
 
 servidor.get('/', (req, res)=>{
-    return res.send('Servidor está ok!')
+    return res.send('Servidor está ok! Nodemon configurado com o TypeScript')
 })
 servidor.get('/usuarios', (req, res)=>{
     return res.send('Servidor está ok!')
