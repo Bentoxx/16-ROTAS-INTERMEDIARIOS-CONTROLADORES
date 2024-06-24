f9f0ebb6ff1168a46361da9be26a085f207ad709
Bento Netto <bentoeemanuele@gmail.com>
2024-06-24 14:59:11 -0300

    Adicionada a bilbioteca nodemon para que o servidor fosse reiniciado automaticamente quando for salva alguma alteração no código

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,9 @@ import express from 'express'
 const servidor = express()
 
 servidor.get('/', (req, res)=>{
-    console.log(req)
     return res.send('Servidor está ok!')
 })
 servidor.get('/usuarios', (req, res)=>{
-    console.log(req)
     return res.send('Servidor está ok!')
 })
 servidor.listen(process.env.PORT)
