77e14ba85ed3d551436630c75c501e66dd62746e
Bento Netto <bentoeemanuele@gmail.com>
2024-06-25 16:01:22 -0300

    Criei arquivo para armazenamento dos constoroladores com o objetivo de garantir uma maior clareza na interpretação do código e passo agora a instanciá-los por importaçãocom o express

---
+++ src/controladores.ts
@@ -0,0 +1,38 @@
+import {Request, Response} from 'express'
+//CADASTRO DE DADOS PARA CONSULTA
+const pessoas = [ 
+    {nome: 'Bento',email: 'bento@email.com'},
+    {nome: 'Mel',email: 'mel@email.com'},
+    {nome: 'Emanuele',email: 'manu@email.com'},
+]
+export const itemProdutos = (req: Request, res:Response)=>{
+//FUNÇÃO DE BUSCA DE PRODUTOS POR PARÂMETRO DE REQUISIÇÃO 
+    console.log(req.params.item); 
+    return res.send('Servidor está ok! Nodemon configurado com o TypeScript')
+}
+export const buscarUsuario = (req: Request,res: Response) => {
+//FUNÇÃO DE BUSCA DE USUÁRIOS CADASTRADOS NO ARRAY PESSOAS ATRAVÉZ DE PARÂMETRO DE REQUISIÇÃO
+    const {email} = req.params
+    const pessoa = pessoas.find((item)=>{
+        return item.email === email
+    })
+    if (!pessoa){
+        return res.send('Pessoa não encontrada!')
+    }
+    return res.send(pessoa)
+}
+export const buscarUsuarioQuery = (req: Request, res: Response) => {
+    //FUNÇÃO DE BUSCA DE USUÁRIOS CADASTRADOS NO ARRAY PESSOAS ATRAVÉZ DE CONSULTA DE REQUISIÇÃO
+        const {email} = req.query
+        if (!email){
+            return res.send('parametro não informado!')
+        }
+        const pessoa = pessoas.find((item)=>{
+            return item.email === email
+        })
+        if(!pessoa) {
+            return res.send('Pessoa não encontrada!')
+        }
+        return res.send(pessoa)
+    
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
+import { buscarUsuario, buscarUsuarioQuery, itemProdutos } from './controladores'
 
 const servidor = express()
+//ROTEADOR
+servidor.get('/produtos/:item', itemProdutos)
+servidor.get('/usuarios/:email', buscarUsuario)
+servidor.get('/usuarios', buscarUsuarioQuery)
 
-servidor.get('/produtos/:item', (req, res)=>{
-    console.log(req.params); 
-    return res.send('Servidor está ok! Nodemon configurado com o TypeScript')
-})
-const pessoas = [
-    {nome: 'Bento',email: 'bento@email.com'},
-    {nome: 'Mel',email: 'mel@email.com'},
-    {nome: 'Emanuele',email: 'manu@email.com'},
-]
-servidor.get('/usuarios', (req, res)=>{
-    const {email} = req.query
-    if (!email){
-        return res.send('parametro não informado!')
-    }
-    const pessoa = pessoas.find((item)=>{
-        return item.email === email
-    })
-    if(!pessoa) {
-        return res.send('Pessoa não encontrada!')
-    }
-    return res.send(pessoa)
-})
 servidor.listen(process.env.PORT)
